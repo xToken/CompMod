5aaea9c80e284f8b8cebbc140331400cb380485b
Matt <mkreider@gmail.com>
2014-08-29 20:02:05 -0400

    Lesson of the day
    Dont code lua and C++ at the same time

--- lua/CompMod/Shared/BalanceAdjustments.lua
+++ lua/CompMod/Shared/BalanceAdjustments.lua
@@ -146,8 +146,7 @@ kBabblersResearchTime = 45				// Comp Mod change, added this.
 //kStompResearchCost = 25				// Comp Mod change, unchanged.
 //kStompResearchTime = 60				// Comp Mod change, unchanged.
 
-//Apply Umbra Changes - Since also modding Umbra DR now, DONT do this.
-[[-
+/*Apply Umbra Changes - Since also modding Umbra DR now, DONT do this.
 local kUmbraModifier = GetUpValue( UmbraMixin.ModifyDamageTaken,   "kUmbraModifier" )
 kUmbraModifier["Shotgun"] = kUmbraShotgunModifier
 kUmbraModifier["Rifle"] = kUmbraBulletModifier
@@ -156,6 +155,7 @@ kUmbraModifier["Sentry"] = kUmbraBulletModifier
 kUmbraModifier["HeavyMachineGun"] = kUmbraBulletModifier
 kUmbraModifier["Minigun"] = kUmbraMinigunModifier
 kUmbraModifier["Railgun"] = kUmbraRailgunModifier
--]]
+*/
+
 //Fade swipe damage.
 //SwipeBlink.kDamage = kSwipeDamage		// Really? Move a global into a Global?
