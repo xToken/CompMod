1b3a01b4ad982ec564b14f0c43ccfc6a37f3b3e0
Matt <mkreider@gmail.com>
2015-01-22 15:34:51 -0500

    Updates for 273

--- lua/InputHandler.lua
+++ lua/InputHandler.lua
@@ -81,6 +81,7 @@ local _keyBinding =
     OverHeadZoomDecrease = InputKey.MouseWheelDown,
     OverHeadZoomReset = InputKey.G,
     PreviousLocationCom = InputKey.None,
+    MovementModifierCom = InputKey.LeftShift,
     Q = InputKey.Q,
     W = InputKey.W,
     E = InputKey.E,
@@ -477,7 +478,7 @@ local function GenerateMove()
             move.hotkey = Move.P
         end  
         
-        if PlayerUI_IsACommander() and _keyState[ _keyBinding.MovementModifier ] then
+        if PlayerUI_IsACommander() and _keyState[ _keyBinding.MovementModifierCom ] then
 
             if _keyState[ _keyBinding.W ] then
                 move.commands = bit.bor(move.commands, Move.ScrollForward)
