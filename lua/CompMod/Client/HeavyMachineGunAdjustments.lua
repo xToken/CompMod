3d9d2f91a27ce090af7c7528828cf8faa6875c84
Matt <mkreider@gmail.com>
2015-03-27 16:46:46 -0400

    Fixes for NS2+

--- lua/CompMod/Client/HeavyMachineGunAdjustments.lua
+++ lua/CompMod/Client/HeavyMachineGunAdjustments.lua
@@ -2,12 +2,12 @@
 Script.Load("lua/Hud/GUIEvent.lua")
 
 local function SetupGUIMarineBuymenu()
-	local GetSmallIconPixelCoordinates 		= GetUpValue( GUIMarineBuyMenu._InitializeEquipped,   "GetSmallIconPixelCoordinates", 			{ LocateRecurse = true } )
-	local GetBigIconPixelCoords 		= GetUpValue( GUIMarineBuyMenu._InitializeContent,   "GetBigIconPixelCoords", 			{ LocateRecurse = true } )
+	local GetSmallIconPixelCoordinates 		= GetUpValue( GUIMarineBuyMenu._InitializeEquipped, "GetSmallIconPixelCoordinates", { LocateRecurse = true } )
+	local GetBigIconPixelCoords 		= GetUpValue( GUIMarineBuyMenu._InitializeContent, "GetBigIconPixelCoords", { LocateRecurse = true } )
 	GetSmallIconPixelCoordinates(kTechId.Rifle)
 	GetBigIconPixelCoords(kTechId.Rifle)
-	local gSmallIconIndex 		= GetUpValue( GetSmallIconPixelCoordinates,   "gSmallIconIndex" )
-	local gBigIconIndex 		= GetUpValue( GetBigIconPixelCoords,   "gBigIconIndex" )
+	local gSmallIconIndex 		= GetUpValue( GetSmallIconPixelCoordinates, "gSmallIconIndex" )
+	local gBigIconIndex 		= GetUpValue( GetBigIconPixelCoords, "gBigIconIndex" )
 	gBigIconIndex[kTechId.HeavyMachineGun] = 15
 	gSmallIconIndex[kTechId.HeavyMachineGun] = 48
 end
@@ -17,12 +17,12 @@ AddPostInitOverride("GUIMarineBuyMenu", SetupGUIMarineBuymenu)
 //Build table, again sigh.
 MarineBuy_GetWeaponDescription(kTechId.Rifle)
 //Grab table
-local gWeaponDescription = GetUpValue( MarineBuy_GetWeaponDescription,   "gWeaponDescription" )
+local gWeaponDescription = GetUpValue( MarineBuy_GetWeaponDescription, "gWeaponDescription" )
 //Add HMG
 gWeaponDescription[kTechId.HeavyMachineGun] = "Heavy Machine Gun"
 
 //This isnt actually loaded as a GUIScript, more as a resource.
-local GetUnlockIconParams = GetUpValue( GUIEvent.UpdateUnlockDisplay,   "GetUnlockIconParams" )
+local GetUnlockIconParams = GetUpValue( GUIEvent.UpdateUnlockDisplay, "GetUnlockIconParams" )
 //Build table
 GetUnlockIconParams(kTechId.Armor1)
 //Get the table
@@ -31,13 +31,13 @@ local kUnlockIconParams = GetUpValue( GetUnlockIconParams,   "kUnlockIconParams"
 kUnlockIconParams[kTechId.HeavyMachineGunTech] = { description = "Heavy Machine Gun Researched" }
 
 //Build table
-local GetDisplayTechId = GetUpValue( MarineBuy_GetEquipped,   "GetDisplayTechId")
+local GetDisplayTechId = GetUpValue( MarineBuy_GetEquipped, "GetDisplayTechId")
 GetDisplayTechId(kTechId.Rifle)
 local gDisplayTechs = GetUpValue( GetDisplayTechId,   "gDisplayTechs")
 //Add HMG
 gDisplayTechs[kTechId.HeavyMachineGun] = true
 
-local kMarineBuyMenuSounds = GetUpValue( MarineBuy_OnItemSelect,   "kMarineBuyMenuSounds")
+local kMarineBuyMenuSounds = GetUpValue( MarineBuy_OnItemSelect, "kMarineBuyMenuSounds")
 local oldMarineBuy_OnItemSelect = MarineBuy_OnItemSelect
 function MarineBuy_OnItemSelect(techId)
 	if techId == kTechId.HeavyMachineGun then
@@ -48,21 +48,21 @@ function MarineBuy_OnItemSelect(techId)
 end
 
 local function SetupActionIcons()
-	local kIconOffsets = GetUpValue( GUIActionIcon.ShowIcon,   "kIconOffsets")
+	local kIconOffsets = GetUpValue( GUIActionIcon.ShowIcon, "kIconOffsets")
 	kIconOffsets["HeavyMachineGun"] = 9
 end
 
 AddPostInitOverride("GUIActionIcon", SetupActionIcons)
 
 local function SetupAmmoColor()
-	local kAmmoColors = GetUpValue( GUIInsight_PlayerHealthbars.UpdatePlayers,   "kAmmoColors")
+	local kAmmoColors = GetUpValue( GUIInsight_PlayerHealthbars.UpdatePlayers,   "kAmmoColors", { LocateRecurse = true })
 	kAmmoColors["heavymachinegun"] = Color(1,0,0,1)
 end
 
 AddPostInitOverride("GUIInsight_PlayerHealthbars", SetupAmmoColor)
 
 local function SetupPickupOffset()
-	local kPickupTextureYOffsets = GetUpValue( GUIPickups.Update,   "kPickupTextureYOffsets", 			{ LocateRecurse = true } )
+	local kPickupTextureYOffsets = GetUpValue( GUIPickups.Update, "kPickupTextureYOffsets", { LocateRecurse = true } )
 	kPickupTextureYOffsets["HeavyMachineGun"] = 12
 end
 
@@ -82,7 +82,7 @@ end
 
 local function SetupInsightShit()
 	local iconSize = Vector(32,32,0)
-	local kIconCoords = GetUpValue( GUIInsight_PlayerFrames.UpdatePlayer,   "kIconCoords" )
+	local kIconCoords = GetUpValue( GUIInsight_PlayerFrames.UpdatePlayer, "kIconCoords", { LocateRecurse = true } )
 	kIconCoords["HMG"] = { 4*iconSize.x, 0, 5*iconSize.x, iconSize.y }
 end
 
