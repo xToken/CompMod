77a527ed2a674e216cfb564ccc40023ab2f35b8f
Matt <mkreider@gmail.com>
2015-12-27 14:09:49 -0500

    Consolidated files

--- lua/CompMod/Shared/MiscAlienAdjustments.lua
+++ lua/CompMod/Shared/MiscAlienAdjustments.lua
@@ -11,6 +11,40 @@ function Egg:GetTechButtons(techId)
     return techButtons
 end
 
+local originalAlienGetPlayerStatusDesc
+originalAlienGetPlayerStatusDesc = Class_ReplaceMethod("Alien", "GetPlayerStatusDesc",
+	function(self)
+	
+		local status = kPlayerStatus.Void
+		
+		if (self:GetIsAlive() == false) then
+			status = kPlayerStatus.Dead
+		else
+			if (self:isa("Embryo")) then
+				if self.gestationTypeTechId == kTechId.Skulk then
+					status = kPlayerStatus.SkulkEgg
+				elseif self.gestationTypeTechId == kTechId.Gorge then
+					status = kPlayerStatus.GorgeEgg
+				elseif self.gestationTypeTechId == kTechId.Lerk then
+					status = kPlayerStatus.LerkEgg
+				elseif self.gestationTypeTechId == kTechId.Fade then
+					status = kPlayerStatus.FadeEgg
+				elseif self.gestationTypeTechId == kTechId.Onos then
+					status = kPlayerStatus.OnosEgg
+				else
+					status = kPlayerStatus.SkulkEgg
+				end
+			else
+				status = kPlayerStatus[self:GetClassName()]
+			end
+		end
+		
+		return status
+
+	end
+
+)
+
 /*
 function BabblerEggAbility:IsAllowed(player)
 	return GetHasTech(player, kTechId.BabblerTech)
