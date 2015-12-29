0cda4fe897731f3126e1af93d944290a726b7c28
Matt <mkreider@gmail.com>
2015-12-29 16:39:43 -0500

    Fix for RTs taking damage at round start
    sometimes

--- lua/CompMod/Server/AlienTechTreeAdjustments.lua
+++ lua/CompMod/Server/AlienTechTreeAdjustments.lua
@@ -239,3 +239,30 @@ oldAlienTeamOnHiveDestroyed = Class_ReplaceMethod("AlienTeam", "OnHiveDestroyed"
 		UpdateEveryCystMaxHP()
 	end
 )
+
+//Fix for Harvester's take a couple ticks of damage at round start sometimes.  Infestation updates on a slower HZ, so run one manually here.
+local function CreateCysts(hive, harvester, teamNumber)
+
+	local hiveOrigin = hive:GetOrigin()
+	local harvesterOrigin = harvester:GetOrigin()
+	
+	// Spawn all the Cyst spawn points close to the hive.
+	local dist = (hiveOrigin - harvesterOrigin):GetLength()
+	for c = 1, #Server.cystSpawnPoints do
+	
+		local spawnPoint = Server.cystSpawnPoints[c]
+		if (spawnPoint - hiveOrigin):GetLength() <= (dist * 1.5) then
+		
+			local cyst = CreateEntityForTeam(kTechId.Cyst, spawnPoint, teamNumber, nil)
+			cyst:SetConstructionComplete()
+			cyst:SetInfestationFullyGrown()
+			cyst:SetImmuneToRedeploymentTime(1)
+			cyst:UpdateInfestation()
+			
+		end
+		
+	end
+	
+end
+
+ReplaceLocals(AlienTeam.SpawnInitialStructures, { CreateCysts = CreateCysts })
