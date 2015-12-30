dd1a15d50784be547958a46fbed701568be8947b
Matt <mkreider@gmail.com>
2015-12-30 02:08:38 -0500

    Updating header

--- lua/CompMod/Shared/PlayerSpawnAdjustments.lua
+++ lua/CompMod/Shared/PlayerSpawnAdjustments.lua
@@ -1,5 +1,7 @@
-// Natural Selection 2
-// lua\PlayerSpawnAdjustments.lua
+// Natural Selection 2 Competitive Mod
+// Source located at - https://github.com/xToken/CompMod
+// Detailed breakdown of changes at https://docs.google.com/document/d/1YOnjJz6_GhioysLaWiRfc17xnrmw6AEJIb6gq7TX3Qg/edit?pli=1
+// lua\CompMod\Shared\PlayerSpawnAdjustments.lua
 // - Dragon
 
 Script.Load("lua/Class.lua")
