13df3bfefcf4df49d0ff262c5a8d655c803bfa12
Matt <mkreider@gmail.com>
2016-01-03 22:56:18 -0500

    Version Increment

--- lua/CompMod_Shared.lua
+++ lua/CompMod_Shared.lua
@@ -10,7 +10,7 @@ Script.Load( "lua/Class.lua" )
 Script.Load( "lua/CompMod/Elixer_Utility.lua" )
 Elixer.UseVersion( 1.72 )
 
-kCompModVersion = 6
+kCompModVersion = 7
 
 local ModFiles = { }
 Shared.GetMatchingFileNames( "lua/CompMod/NewTech/*.lua", true, ModFiles )
