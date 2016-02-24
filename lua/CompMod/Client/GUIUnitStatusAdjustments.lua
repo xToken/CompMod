b8bbb1925c881431c0d44ade447ae9a60588f896
Matt <mkreider@gmail.com>
2016-02-23 19:06:21 -0500

    LETSSSSSS GO

--- lua/CompMod/Client/GUIUnitStatusAdjustments.lua
+++ lua/CompMod/Client/GUIUnitStatusAdjustments.lua
@@ -199,7 +199,7 @@ local function SetupBuildVersionTags()
 	originalFeedbackInit = Class_ReplaceMethod("GUIFeedback", "Initialize",
 	function(self)
 		originalFeedbackInit(self)
-		self.buildText:SetText(self.buildText:GetText() .. " (" .. "CompMod" .. " v"  .. kCompModVersion .. "R4 )")
+		self.buildText:SetText(self.buildText:GetText() .. " (" .. "CompMod" .. " v"  .. kCompModVersion .. "r4 )")
 	end)
 	
 end
