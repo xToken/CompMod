29c39d0635dd9ee2ce59e60dc8595ebe2b6bdd17
Dragon <mkreider@gmail.com>
2018-12-08 13:57:39 -0500

    Fixes for power nodes
    Hidden from map/unit status
    Removed some of the effects

--- lua/CompMod/Shared/PowerPointAdjustments.lua
+++ lua/CompMod/Shared/PowerPointAdjustments.lua
@@ -1,17 +1,40 @@
 -- Natural Selection 2 Competitive Mod
 -- Source located at - https://github.com/xToken/CompMod
 -- Detailed breakdown of changes at https://docs.google.com/document/d/1YOnjJz6_GhioysLaWiRfc17xnrmw6AEJIb6gq7TX3Qg/edit?pli=1
--- lua\CompMod\Shared\MarineStructureAdjustments.lua
+-- lua\CompMod\Shared\PowerPointAdjustments.lua
 -- - Dragon
 
+-- Prevent damaging the power nodes
 function PowerPoint:GetCanTakeDamageOverride()
     return false
 end
 
+function PowerPoint:OnTakeDamage()
+end
+
+function PowerPoint:DoDamageLighting()
+end
+
+-- Things are always powered.
 function PowerConsumerMixin:GetIsPowered() 
     return true
 end
 
+-- Prevent minimap blip?
+local oldMapBlipMixinGetMapBlipInfo = MapBlipMixin.GetMapBlipInfo
+function MapBlipMixin:GetMapBlipInfo()
+	if self:isa("PowerPoint") then
+		return false, kMinimapBlipType.Undefined, -1, false, false
+	end
+	return oldMapBlipMixinGetMapBlipInfo(self)
+end
+
+-- Prevent unit status?
+function PowerPoint:GetShowUnitStatusForOverride()
+    return false
+end
+
+-- Always socketed/active/use unsockeded model
 if Server then
 	local kUnsocketedSocketModelName = GetUpValue( PowerPoint.SetInternalPowerState, "kUnsocketedSocketModelName" )
 	local kUnsocketedAnimationGraph
