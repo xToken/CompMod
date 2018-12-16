842895141d6e3309e91715ea6d2e8d09f1dc1b4f
Dragon <mkreider@gmail.com>
2018-12-15 22:15:31 -0500

    Remove poison bite

---
+++ lua/CompMod/Shared/Weapons/Alien/LerkBite.lua
@@ -0,0 +1,39 @@
+-- Natural Selection 2 Competitive Mod
+-- Source located at - https://github.com/xToken/CompMod
+-- lua\CompMod\Shared\LerkBite.lua
+-- - Dragon
+
+function LerkBite:OnTag(tagName)
+
+    PROFILE("LerkBite:OnTag")
+
+    if tagName == "hit" then
+    
+        local player = self:GetParent()
+        
+        if player then
+        
+            self:TriggerEffects("lerkbite_attack")
+            self:OnAttack(player)
+            
+            self.spiked = false
+        
+            local didHit, target, endPoint, surface = AttackMeleeCapsule(self, player, kLerkBiteDamage, self:GetRange(), nil, false, EntityFilterOneAndIsa(player, "Babbler"))
+            
+            if didHit and target then
+            
+                if Client then
+                    self:TriggerFirstPersonHitEffects(player, target)
+                end
+            
+            end
+            
+            if target and HasMixin(target, "Live") and not target:GetIsAlive() then
+                self:TriggerEffects("bite_kill")
+            end
+            
+        end
+        
+    end
+    
+end
