2ae6d9c9c3e481c1a859a11febfd38ce1b6b5726
Dragon <mkreider@gmail.com>
2018-12-15 22:17:30 -0500

    Removal of old files
    Restructoring mod files..
    Too many to just keep in shared/client/server directories
    This removes all the old files

--- lua/CompMod/Shared/MarineMovementAdjustments.lua
+++
@@ -1,9 +0,0 @@
--- Natural Selection 2 Competitive Mod
--- Source located at - https://github.com/xToken/CompMod
--- Detailed breakdown of changes at https://docs.google.com/document/d/1YOnjJz6_GhioysLaWiRfc17xnrmw6AEJIb6gq7TX3Qg/edit?pli=1
--- lua\CompMod\Shared\MarineMovementAdjustments.lua
--- - Dragon
-
--- Disable Sprint
-function SprintMixin:UpdateSprintingState(input)
-end
