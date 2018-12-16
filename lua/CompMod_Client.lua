b5cadfd8711e3e362b6c8b1b660215fbde06f0fd
Dragon <mkreider@gmail.com>
2018-12-15 22:12:32 -0500

    Updated modloader details
    Mainly just a cleanup effort

--- lua/CompMod_Client.lua
+++ lua/CompMod_Client.lua
@@ -1,6 +1,5 @@
 -- Natural Selection 2 Competitive Mod
 -- Source located at - https://github.com/xToken/CompMod
--- Detailed breakdown of changes at https://docs.google.com/document/d/1YOnjJz6_GhioysLaWiRfc17xnrmw6AEJIb6gq7TX3Qg/edit?pli=1
 -- lua\CompMod_Client.lua
 -- - Dragon
 
@@ -10,19 +9,10 @@
 Script.Load("lua/CompMod_Shared.lua")
 
 -- Load client specific changes
-local PreLoad = { }
-
--- Load this first, enables other client functions to override GUI in a slightly less horribly awful way
-for i = 1, #PreLoad do
-	Script.Load(PreLoad[i])
-end
-
 local MainFiles = { }
 Shared.GetMatchingFileNames( "lua/CompMod/Client/*.lua", true, MainFiles )
 
 -- Load function changes
 for i = 1, #MainFiles do
-	if not table.contains(PreLoad, MainFiles[i]) then
-		Script.Load(MainFiles[i])
-	end	
+	Script.Load(MainFiles[i])
 end
--- lua/CompMod_FileHooks.lua
+++ lua/CompMod_FileHooks.lua
@@ -1,11 +1,16 @@
 -- Natural Selection 2 Competitive Mod
 -- Source located at - https://github.com/xToken/CompMod
--- Detailed breakdown of changes at https://docs.google.com/document/d/1YOnjJz6_GhioysLaWiRfc17xnrmw6AEJIb6gq7TX3Qg/edit?pli=1
 -- lua\CompMod_FileHooks.lua
 -- - Dragon
 
 -- Dont run from filehook calls on main menu.
 if not string.find(Script.CallStack(), "Main.lua") then
 	-- Setup file hooks here
-	ModLoader.SetupFileHook( "lua/GUIFeedback.lua", "lua/CompMod/Post/GUIBuildVersion.lua", "post" )
+	ModLoader.SetupFileHook( "lua/TechTreeConstants.lua", "lua/CompMod/FileHooks/Post/TechTreeConstants.lua", "post" )
+	ModLoader.SetupFileHook( "lua/TechData.lua", "lua/CompMod/FileHooks/Post/TechData.lua", "post" )
+	ModLoader.SetupFileHook( "lua/Balance.lua", "lua/CompMod/FileHooks/Post/Balance.lua", "post" )
+	
+	ModLoader.SetupFileHook( "lua/GUIFeedback.lua", "lua/CompMod/FileHooks/Post/GUIFeedback.lua", "post" )
+	ModLoader.SetupFileHook( "lua/Hud/Marine/GUIMarineHUD.lua", "lua/CompMod/FileHooks/Post/GUIMarineHud.lua", "post" )
+	
 end
