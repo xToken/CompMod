f44b6328afce13f4bdbd57cea568d72989664660
Dragon <mkreider@gmail.com>
2018-12-15 22:22:36 -0500

    Alien Khamm abilities
    Moved over from drifter to global

---
+++ lua/CompMod/Shared/MucousMembrane.lua
@@ -0,0 +1,18 @@
+-- Natural Selection 2 Competitive Mod
+-- Source located at - https://github.com/xToken/CompMod
+-- lua\CompMod\Shared\MucousMembrane.lua
+-- - Dragon
+
+MucousMembrane.kRadius = kMucousMembraneRadius
+
+function MucousMembrane:OnInitialized()
+    
+    if Server then
+        -- sound feedback
+        self:TriggerEffects("enzyme_cloud")
+        DestroyEntitiesWithinRange("MucousMembrane", self:GetOrigin(), 5, EntityFilterOne(self)) 
+    end
+    
+    CommanderAbility.OnInitialized(self)
+
+end
