5685abf47ea71e9556456ed6f299131409e36564
Dragon <mkreider@gmail.com>
2018-12-15 22:28:15 -0500

    Onos speed
    From R6 Alpha

---
+++ lua/CompMod/Shared/Classes/Alien/Onos.lua
@@ -0,0 +1,7 @@
+-- Natural Selection 2 Competitive Mod
+-- Source located at - https://github.com/xToken/CompMod
+-- lua\CompMod\Shared\Onos.lua
+-- - Dragon
+
+Onos.kMaxSpeed = kOnosMaxGroundSpeed
+Onos.kChargeSpeed = kOnosMaxChargeSpeed
