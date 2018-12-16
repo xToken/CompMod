48ee83193175fa8224c404b19b161d66fc13bd0f
Dragon <mkreider@gmail.com>
2018-12-15 22:23:00 -0500

    Powerpoint file moved
    Doh

---
+++ lua/CompMod/Client/PowerPoint.lua
@@ -0,0 +1,13 @@
+-- Natural Selection 2 Competitive Mod
+-- Source located at - https://github.com/xToken/CompMod
+-- lua\CompMod\Client\PowerPoint.lua
+-- - Dragon
+
+function PowerPoint:OnUpdateRender()
+end
+
+function PowerPoint:CreateImpulseStructureEffect()
+end
+
+function PowerPoint:CreateImpulseEffect()
+end
