0e5790fd722b6593f19c9eaf4cceba07d991347c
Dragon <mkreider@gmail.com>
2018-12-15 22:19:25 -0500

    Comm station updates
    Removed stuff - nanoshield/catpacks

---
+++ lua/CompMod/Shared/Structures/Marine/CommandStation.lua
@@ -0,0 +1,8 @@
+-- Natural Selection 2 Competitive Mod
+-- Source located at - https://github.com/xToken/CompMod
+-- lua\CompMod\Shared\CommandStation.lua
+-- - Dragon
+
+function CommandStation:GetTechButtons()
+    return { kTechId.PowerSurgeTech }
+end
