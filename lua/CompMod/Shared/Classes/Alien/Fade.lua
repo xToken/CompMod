a99eac6c8d21a35474b94afb863aa87301eb8747
Dragon <mkreider@gmail.com>
2018-12-15 22:28:37 -0500

    Fade speed
    from R6 Alpha

---
+++ lua/CompMod/Shared/Classes/Alien/Fade.lua
@@ -0,0 +1,8 @@
+-- Natural Selection 2 Competitive Mod
+-- Source located at - https://github.com/xToken/CompMod
+-- lua\CompMod\Shared\Fade.lua
+-- - Dragon
+
+-- Actually set these values since locals
+ReplaceLocals(Fade.GetMaxSpeed, { kMaxSpeed = kFadeMaxSpeed })
+ReplaceLocals(Fade.GetMaxSpeed, { kBlinkSpeed = kFadeBlinkSpeed })
