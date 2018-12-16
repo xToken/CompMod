ffe268c1a3e1456bc406a47be04b323b4695e6c0
Dragon <mkreider@gmail.com>
2018-12-15 22:15:41 -0500

    Bite cone tweak
    1.2 -> 1.0

---
+++ lua/CompMod/Shared/Weapons/Alien/BiteLeap.lua
@@ -0,0 +1,9 @@
+-- Natural Selection 2 Competitive Mod
+-- Source located at - https://github.com/xToken/CompMod
+-- lua\CompMod\Shared\BiteLeap.lua
+-- - Dragon
+
+function BiteLeap:GetMeleeBase()
+    -- Width of box, height of box
+    return 1.0, 1.2
+end
