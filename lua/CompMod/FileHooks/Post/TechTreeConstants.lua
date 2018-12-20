a0d0b88b7c06948250c167c78ae83897217e7749
Dragon <mkreider@gmail.com>
2018-12-19 21:49:35 -0500

    Splitting TechTreeConstants
    keep it more readable/editable

--- lua/CompMod/FileHooks/Post/TechTreeConstants.lua
+++ lua/CompMod/FileHooks/Post/TechTreeConstants.lua
@@ -6,29 +6,11 @@
 Script.Load( "lua/CompMod/Elixer_Utility.lua" )
 Elixer.UseVersion( 1.8 )
 
+-- Load the file with our new tech names
+Script.Load("lua/CompMod/TechData/TechTreeConstants.lua")
+
 -- Table for new tech names
-local newTechNames = { }
-table.insert(newTechNames, "FlamethrowerUpgrade1")
-table.insert(newTechNames, "ShotgunUpgrade1")
-table.insert(newTechNames, "ShotgunUpgrade2")
-table.insert(newTechNames, "MGUpgrade1")
-table.insert(newTechNames, "MGUpgrade2")
-table.insert(newTechNames, "GLUpgrade1")
-table.insert(newTechNames, "JetpackUpgrade1")
-table.insert(newTechNames, "ExoUpgrade1")
-table.insert(newTechNames, "ExoUpgrade2")
-table.insert(newTechNames, "MinigunUpgrade1")
-table.insert(newTechNames, "MinigunUpgrade2")
-table.insert(newTechNames, "RailgunUpgrade1")
-table.insert(newTechNames, "ARCUpgrade1")
-table.insert(newTechNames, "ARCUpgrade2")
-table.insert(newTechNames, "SentryUpgrade1")
-table.insert(newTechNames, "Weapons1Upgrade")
-table.insert(newTechNames, "Weapons2Upgrade")
-table.insert(newTechNames, "Weapons3Upgrade")
-table.insert(newTechNames, "Armor1Upgrade")
-table.insert(newTechNames, "Armor2Upgrade")
-table.insert(newTechNames, "Armor3Upgrade")
+local newTechNames = BuildCompModTechTreeConstantUpdates()
 
 -- Grab existing list of techID names
 local gTechIdToString = GetUpValue( StringToTechId, "gTechIdToString", { LocateRecurse = true } )
