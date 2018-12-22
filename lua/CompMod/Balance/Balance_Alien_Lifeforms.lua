44e63f69f6b51a76a74dde5130fa68b67f08cf99
Dragon <mkreider@gmail.com>
2018-12-21 19:48:22 -0500

    Removed spikes damage falloff

--- lua/CompMod/Balance/Balance_Alien_Lifeforms.lua
+++ lua/CompMod/Balance/Balance_Alien_Lifeforms.lua
@@ -13,9 +13,3 @@ kGorgeUpgradeCost = 1						-- Default from 1
 kLerkUpgradeCost = 2						-- Decreased from 3
 kFadeUpgradeCost = 3						-- Decreased from 5
 kOnosUpgradeCost = 4						-- Decreased from 8
-
--- SPIKES
-kSpikeMaxDamage = 7							-- Default is 7
-kSpikeMinDamage = 5							-- Decreased from 7
-kSpikeMinDamageRange = 7					-- Decreased from 9
-kSpikeMaxDamageRange = 2					-- Default is 2
