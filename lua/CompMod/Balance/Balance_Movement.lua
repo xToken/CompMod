571ae6aeedaa1d4a45b1088eb2616104696f061c
Dragon <mkreider@gmail.com>
2018-12-23 19:21:36 -0500

    Marine movement nerf
    Marine speed - 0.1
    JP Base fly speed - 0.5

--- lua/CompMod/Balance/Balance_Movement.lua
+++ lua/CompMod/Balance/Balance_Movement.lua
@@ -22,11 +22,11 @@ kOnosMaxGroundSpeed = 7.0					-- Increased from 6.6
 kOnosMaxChargeSpeed = 13.0					-- Increased from 12.5								
 
 -- Marine
-kMarineWalkMaxSpeed = 5.8					-- Increased from 5.0
+kMarineWalkMaxSpeed = 5.7					-- Increased from 5.0
 kMarineMaxSlowWalkSpeed = 2.9
 
 -- JetpackMarine
-kJetpackFlySpeed = 11						-- Increased from 9.0
+kJetpackFlySpeed = 10.5						-- Increased from 9.0
 
 -- Jetpack
 kJetpackingAccel = 0.95
