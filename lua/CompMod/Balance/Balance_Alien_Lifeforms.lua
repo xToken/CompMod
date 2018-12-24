6f9305de6a78ab6afcc3f5ebeddab1de71fbc638
Dragon <mkreider@gmail.com>
2018-12-23 19:20:10 -0500

    Onos cost decrease

--- lua/CompMod/Balance/Balance_Alien_Lifeforms.lua
+++ lua/CompMod/Balance/Balance_Alien_Lifeforms.lua
@@ -6,7 +6,7 @@
 kGorgeCost = 10								-- Decreased from 8
 kLerkCost = 15								-- Decreased from 21
 kFadeCost = 25								-- Decreased from 37
-kOnosCost = 40								-- Decreased from 62
+kOnosCost = 35								-- Decreased from 62
 
 kSkulkUpgradeCost = 0						-- Default is 0
 kGorgeUpgradeCost = 1						-- Default from 1
