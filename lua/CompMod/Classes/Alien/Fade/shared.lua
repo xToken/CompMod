31821e979a5aeaee33afcb544ce38bd08b895bad
Dragon <mkreider@gmail.com>
2019-01-09 19:39:47 -0500

    Fade ceiling fix 3.0

--- lua/CompMod/Classes/Alien/Fade/shared.lua
+++ lua/CompMod/Classes/Alien/Fade/shared.lua
@@ -8,7 +8,7 @@ function Fade:GetGroundTransistionTime()
 end
 
 local kFadeOffsetSpeed = 5
-local kFadeModelOffset = 0.70
+local kFadeModelOffsetAmount = 0.70
 
 local originalFadeOnCreate
 originalFadeOnCreate = Class_ReplaceMethod("Fade", "OnCreate",
@@ -27,38 +27,37 @@ originalFadeModifyVelocity = Class_ReplaceMethod("Fade", "ModifyVelocity",
 
 		-- Model offset for crouching blinking fades		
 		-- Trace up to make sure we are against the ceiling.
-		-- Default to no updates
-		local origin = self:GetOrigin()
 		self.updateOffset = false
-		local upTrace = Shared.TraceRay(origin, origin + Vector(0, 2, 0), CollisionRep.Move, PhysicsMask.AllButPCs, EntityFilterOneAndIsa(self, "Babbler"))
-		-- This seems to return ghosted traces... so minimum trace to ensure we actually found SOMETHING above us thats REALLLY there
-		if upTrace.fraction > 0.3 and upTrace.fraction < 0.8 then
-			-- The ceiling is here.
-			-- Trace down to make sure we are not against the floor.
-			local downTrace = Shared.TraceRay(origin, origin - Vector(0, kFadeModelOffset + 0.1, 0), CollisionRep.Move, PhysicsMask.AllButPCs, EntityFilterOne(self))
-			if downTrace.fraction == 1 then
-				-- Not against the floor, update offset
-				self.updateOffset = true
+		local crouchoffset = self:GetCrouchAmount()
+
+		-- Only update if we are crouching, or if our model is offset already
+		if crouchoffset > 0 or self.modelOffset > 0 then
+			local origin = self:GetOrigin()
+			local maxoffset = math.min(crouchoffset - self:ModifyCrouchAnimation(crouchoffset), 1)
+			local upTrace = Shared.TraceRay(origin, origin + Vector(0, 2, 0), CollisionRep.Move, PhysicsMask.AllButPCs, EntityFilterOneAndIsa(self, "Babbler"))
+
+			-- This seems to return ghosted traces... so minimum trace to ensure we actually found SOMETHING above us thats REALLLY there
+			if upTrace.fraction > 0.3 and upTrace.fraction < 0.8 then
+				-- The ceiling is here.
+				-- Trace down to make sure we are not against the floor.
+				local downTrace = Shared.TraceRay(origin, origin - Vector(0, kFadeModelOffsetAmount + 0.1, 0), CollisionRep.Move, PhysicsMask.AllButPCs, EntityFilterOneAndIsa(self, "Babbler"))
+				if downTrace.fraction == 1 then
+					-- Not against the floor, update offset
+					self.updateOffset = true
+				end
 			end
+			
+		    if self.updateOffset and self.modelOffset < maxoffset then
+		        self.modelOffset = Clamp(self.modelOffset + (input.time * math.abs(velocity.y) * kFadeOffsetSpeed), 0, maxoffset)
+		    elseif not self.updateOffset and self.modelOffset > 0 then
+		    	self.modelOffset = Clamp(self.modelOffset - (input.time * math.abs(velocity.y) * kFadeOffsetSpeed), 0, maxoffset)
+		    end
 		end
-		
-		local crouchoffset = self:GetCrouchAmount() 
-		local modelcrouchoffset = self:ModifyCrouchAnimation(crouchoffset)
-		local maxoffset = math.min(crouchoffset - modelcrouchoffset, 1)
-	    if crouchoffset > 0 and self.updateOffset then
-	        if self.modelOffset < maxoffset then
-	            self.modelOffset = Clamp(self.modelOffset + (input.time * math.abs(velocity.y) * kFadeOffsetSpeed), 0, maxoffset)
-	        end
-	    else
-	        if self.modelOffset > 0 then
-	            self.modelOffset = Clamp(self.modelOffset - (input.time * math.abs(velocity.y) * kFadeOffsetSpeed), 0, 1)
-	        end
-	    end
 	end
 )
 
 function Fade:OnAdjustModelCoords(modelCoords)
-    modelCoords.origin = modelCoords.origin - Vector(0, self.modelOffset * kFadeModelOffset, 0)
+    modelCoords.origin = modelCoords.origin - Vector(0, self.modelOffset * kFadeModelOffsetAmount, 0)
     return modelCoords
 end
 
