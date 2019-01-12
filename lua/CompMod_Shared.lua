cfef7393b1d0350da9a300c182c39f2f8b72e35c
Dragon <mkreider@gmail.com>
2019-01-12 15:10:26 -0500

    Build 20!

--- lua/CompMod_Shared.lua
+++ lua/CompMod_Shared.lua
@@ -10,7 +10,7 @@ Script.Load("lua/CompMod/Utilities/Elixer/shared.lua")
 
 Elixer.UseVersion( 1.8 )
 
-kCompModBuild = 18
+kCompModBuild = 20
 
 local MainFiles = { }
 Shared.GetMatchingFileNames("lua/CompMod/*shared.lua", true, MainFiles)
