f3beda4c18c43ab5b11bb8d09c369fbd9762f083
Dragon <mkreider@gmail.com>
2019-01-12 15:02:23 -0500

    Babbler eggs dont use sustenance mechanic

---
+++ lua/CompMod/Structures/Alien/BabblerEgg/shared.lua
@@ -0,0 +1,8 @@
+-- Natural Selection 2 Competitive Mod
+-- Source located at - https://github.com/xToken/CompMod
+-- lua\CompMod\Structures\Alien\BabblerEgg\shared.lua
+-- - Dragon
+
+function BabblerEgg:DisableSustenance()
+	return true
+end
