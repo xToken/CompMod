8e9c02f282ff6c28ed200df94c3f87b21f12c6be
Dragon <mkreider@gmail.com>
2019-01-12 15:09:40 -0500

    HP tweaks
    Skulk cara
    Lerk base armor
    Fade HP/armor
    Onos HP/armor
    Regen

--- lua/CompMod/Balance/Balance_Health.lua
+++ lua/CompMod/Balance/Balance_Health.lua
@@ -19,7 +19,7 @@ kNanoArmorHealInterval = 1
 
 kSkulkHealth = 75							-- Vanilla is 75
 kSkulkArmor = 10							-- Vanilla is 10
-kSkulkArmorFullyUpgradedAmount = 35			-- Vanilla is 30
+kSkulkArmorFullyUpgradedAmount = 25			-- Vanilla is 30
 kSkulkHealthPerBioMass = 0					-- Vanilla is 3
 
 kGorgeHealth = 250							-- Vanilla is 160
@@ -28,21 +28,21 @@ kGorgeArmorFullyUpgradedAmount = 90			-- Vanilla is 100
 kGorgeHealthPerBioMass = 0					-- Vanilla is 2
 
 kLerkHealth = 200							-- Vanilla is 150
-kLerkArmor = 30								-- Vanilla is 45
-kLerkArmorFullyUpgradedAmount = 90			-- Vanilla is 60
+kLerkArmor = 20								-- Vanilla is 45
+kLerkArmorFullyUpgradedAmount = 80			-- Vanilla is 60
 kLerkHealthPerBioMass = 0					-- Vanilla is 2
 
-kFadeHealth = 350							-- Vanilla is 250
-kFadeArmor = 70								-- Vanilla is 80
-kFadeArmorFullyUpgradedAmount = 160			-- Vanilla is 120
+kFadeHealth = 300							-- Vanilla is 250
+kFadeArmor = 80								-- Vanilla is 80
+kFadeArmorFullyUpgradedAmount = 170			-- Vanilla is 120
 kFadeHealthPerBioMass = 0					-- Vanilla is 5
 
-kOnosHealth = 900							-- Vanilla is 900
-kOnosArmor = 400							-- Vanilla is 450
+kOnosHealth = 800							-- Vanilla is 900
+kOnosArmor = 450							-- Vanilla is 450
 kOnosArmorFullyUpgradedAmount = 750			-- Vanilla is 650
 kOnosHealtPerBioMass = 0					-- Vanilla is 30
 
-kAlienRegenerationPercentage = 0.12			-- Vanilla is 0.08
+kAlienRegenerationPercentage = 0.09			-- Vanilla is 0.08
 kAlienMinRegeneration = 5					-- Vanilla is 6
 kAlienMaxRegeneration = 120					-- Vanilla is 80
 
@@ -100,6 +100,11 @@ kVeilArmor = 0								-- Vanilla is 0
 kMatureVeilHealth = 1100					-- Vanilla is 1100
 kMatureVeilArmor = 0						-- Vanilla is 0
 
+kCystHealth = 200
+kCystArmor = 0
+kMatureCystHealth = 650
+kMatureCystArmor = 0
+
 kMACHealth = 300							-- Vanilla is 300
 kMACArmor = 0								-- Vanilla is 50
 
@@ -115,17 +120,17 @@ kWebHealth = 40								-- Vanilla is 80
 
 kArmorHealScalar = 1						-- Vanilla is 1
 
-kMaturityGracePeriod = 10					-- How long after the last mist before maturity starts decreasing
+kMaturityGracePeriod = 0					-- How long after the last mist before maturity starts decreasing
 kMaturityStarvingThreshold = 0.3			-- Below this is 'staving'
 kMaturityGrownThreshold = 0.8				-- Above this is 'mature'
 kMaturityMinEffectivenessThreshold = 0.25	-- Minimum of this amount of effectiveness
 kMaturityMaxEffectivenessThreshold = 0.9	-- After this, effectiveness no longer increases
 kMaturityStartingLevel = 0
 
-kMaturityBaseBreakpoint = 1					-- This is the threshold for the maturation/starvation rate.  Above is accelerated, below is base
+kMaturityBaseBreakpoint = 1					-- This is the threshold for the maturation/starvation rate.  Above is accelerated, eq/below is base
 local t = kMaturityBaseBreakpoint
 local q = 1-t
 kMaturityBaseStarvationRate = -t/120		-- This is the base rate that maturity is lost at.
 kMaturityAcceleratedStarvationRate = -q/1	-- This is the accelerated rate that maturity is lost at.
-kMaturityBaseGainRate = t/33				-- This is the base rate that maturity is gained at.
+kMaturityBaseGainRate = t/30				-- This is the base rate that maturity is gained at.
 kMaturityAcceleratedGainRate = q/1			-- This is the accelerated rate that maturity is gained at.
