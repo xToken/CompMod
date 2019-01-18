162d82beeb7ee3c9f14f96e67f884db909338d9f
Dragon <mkreider@gmail.com>
2019-01-18 15:27:36 -0500

    Supply balance vars

--- lua/CompMod/Balance/Balance_Misc.lua
+++ lua/CompMod/Balance/Balance_Misc.lua
@@ -7,3 +7,6 @@ kStructureInfestationRadius = 10				-- Increased from 2, actually utilized
 kHiveInfestationRadius = 25						-- Increased from 20
 
 kPhaseGateDepartureRate = 0.3
+
+kSupplyPerPowerPoint = 10
+kBaseSupply = 100
