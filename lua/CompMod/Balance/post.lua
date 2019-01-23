da05aefffeb5f5b4d4d9e7a6953e98b52af8d7fa
Dragon <mkreider@gmail.com>
2019-01-22 21:15:23 -0500

    Merged misc into globals

--- lua/CompMod/Balance/post.lua
+++ lua/CompMod/Balance/post.lua
@@ -5,12 +5,11 @@
 
 -- Balance.lua loads the other balance files - so just load everything once here.
 Script.Load("lua/CompMod/Utilities/Elixer/shared.lua")
-AppendToEnum(kDamageType, 'QuadStructural')
+AppendToEnum(kDamageType, 'TripleStructural')
 AppendToEnum(kDamageType, 'HalfStructural')
 
 Script.Load( "lua/CompMod/Balance/Balance_Globals.lua" )
 Script.Load( "lua/CompMod/Balance/Balance_Health.lua" )
-Script.Load( "lua/CompMod/Balance/Balance_Misc.lua" )
 Script.Load( "lua/CompMod/Balance/Balance_Alien_Lifeforms.lua" )
 Script.Load( "lua/CompMod/Balance/Balance_Alien_Structures.lua" )
 Script.Load( "lua/CompMod/Balance/Balance_Alien_Tech.lua" )
