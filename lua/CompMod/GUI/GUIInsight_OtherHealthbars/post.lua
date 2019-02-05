a53690967fcec93890b3141ab470d8ad5e79d2df
Dragon <mkreider@gmail.com>
2019-02-05 10:00:42 -0500

    fix file name header

--- lua/CompMod/GUI/GUIInsight_OtherHealthbars/post.lua
+++ lua/CompMod/GUI/GUIInsight_OtherHealthbars/post.lua
@@ -1,6 +1,6 @@
 -- Natural Selection 2 Competitive Mod
 -- Source located at - https://github.com/xToken/CompMod
--- lua\CompMod\GUI\GUIFeedback\post.lua
+-- lua\CompMod\GUI\GUIInsight_OtherHealthbars\post.lua
 -- - Dragon
 
 local kOtherTypes = GetUpValue(GUIInsight_OtherHealthbars.Update, "kOtherTypes")
